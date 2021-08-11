ac88038b0d5d355bf8957376743787d60869094d
Levi <shrieverdev@gmail.com>
2021-08-11 19:49:42 -0400

    added bouncing arrow

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./intro.scss";
-
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 const Intro = () => {
   return (
     <div id='intro' className='intro'>
@@ -9,6 +9,9 @@ const Intro = () => {
         <br />
         Web Developer
       </h1>
+      <a href='#portfolio'>
+        <KeyboardArrowDownIcon className='down-arrow' />
+      </a>
     </div>
   );
 };
