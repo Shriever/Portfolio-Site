fd341c72a346fcf7e691bc9b0119695061c39c5e
Levi <shrieverdev@gmail.com>
2021-08-12 13:40:50 -0400

    added data for projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,39 @@ import React from "react";
 import "./portfolio.scss";
 
 const Portfolio = () => {
-  return <div id="portfolio" className='portfolio'></div>;
+  const projects = [
+    {
+      title: "Quiz App",
+      links: {
+        github: "https://github.com/Shriever/Quiz-App",
+        live: "https://quizapp.gatsbyjs.io/",
+      },
+    },
+    {
+      title: "Compound Interest Calculator",
+      links: {
+        github: "https://github.com/Shriever/Compound-interest-calculator",
+        live: "https://shriever.github.io/Compound-interest-calculator/",
+      },
+    },
+    {
+      title: "Thought Tracker",
+      links: {
+        github: "https://github.com/Shriever/Thought-Tracker",
+        live: null,
+      },
+    },
+  ];
+  return (
+    <div id='portfolio' className='portfolio'>
+      <h1 className='portfolio-heading'>Projects</h1>
+      <div className='container'>
+        {projects.map(el => {
+          return <div className='card'></div>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
