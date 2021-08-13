c382c73c9cfb3c5067a30aa948701d806e3f788e
Levi <shrieverdev@gmail.com>
2021-08-13 14:47:59 -0400

    About section styled

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,7 +5,19 @@ const About = () => {
   return (
     <div id='about' className='about'>
       <h2 className='header'>About Me</h2>
-      <div className="about-body"></div>
+      <div className='about-body'>
+        <p className='about-text'>
+          My name is Levi Lieberman. I love learning about the web and how to
+          solve problems with code. I think that the power of computers is vast
+          and I am always searching out new ways to explore the world of
+          technology.
+        </p>
+        <p className='about-text'>
+          I have taken a number of online courses including Harvard's CS50 and
+          Data Structures &amp; Algorithms on Udemy. Now I'm looking to meet
+          great people in the tech space and take my skills to the next level.
+        </p>
+      </div>
     </div>
   );
 };
