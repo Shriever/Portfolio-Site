a443bc7a8e426d4e89a9c5d22b92d303a8c6b867
Levi Lieberman <ShrieverDev@gmail.com>
2021-10-25 14:21:30 -0700

    added about section to useState

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import "./about.scss";
+import React, { useState } from 'react';
+import './about.scss';
+
+const initialParagraphs = [
+  'My name is Levi Lieberman. I love learning about the web and how to solve problems with code. I think that the power of computers is vast and I am always searching out new ways to explore the world of technology.',
+  "I work with modern technologies like TypeScript and GraphQL to implement clever solutions to everyday problems. I'm always looking to meet great people in the tech space and improve my skills.",
+];
 
 const About = () => {
+  const [paragraphs, setParagraphs] = useState(initialParagraphs);
   return (
     <div id='about' className='about'>
       <h2 className='header'>About Me</h2>
       <div className='about-body'>
-        <p className='about-text'>
-          My name is Levi Lieberman. I love learning about the web and how to
-          solve problems with code. I think that the power of computers is vast
-          and I am always searching out new ways to explore the world of
-          technology.
-        </p>
-        <p className='about-text'>
-          I work with modern technologies like TypeScript and GraphQL to
-          implement clever solutions to everyday problems. I'm looking to meet
-          great people in the tech space and improve my skills.
-        </p>
+        {paragraphs.map((p, idx) => (
+          <p className='about-text' key={idx}>
+            {p}
+          </p>
+        ))}
       </div>
     </div>
   );
