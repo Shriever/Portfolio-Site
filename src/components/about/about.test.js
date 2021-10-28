9b24e9b9382cd223be1a0e68eb8f65c4bbdef917
Levi Lieberman <ShrieverDev@gmail.com>
2021-10-28 16:41:31 -0700

    removed commented test

--- src/components/about/about.test.js
+++ src/components/about/about.test.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import About from './About';
 import { render } from '@testing-library/react';
-// describe('file', () => {
-//   it('loads', () => {
-//     expect(true).toBe(true);
-//   });
-// });
+
 test('should ', () => {
   const screen = render(<About />);
 
