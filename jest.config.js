97a4f6905b08940df4aebcd7d64f3843c0724d7b
Levi Lieberman <ShrieverDev@gmail.com>
2021-10-28 16:16:46 -0700

    added jest deps

--- jest.config.js
+++ jest.config.js
@@ -2,3 +2,21 @@ module.exports = {
   testEnvironment: `jsdom`,
   setupFilesAfterEnv: ['./setup-test-env.js'],
 };
+module.exports = {
+  transform: {
+    '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
+  },
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+  },
+  testEnvironment: `jsdom`,
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  globals: {
+    __PATH_PREFIX__: ``,
+  },
+  testURL: `http://localhost`,
+  setupFiles: [`./loadershim.js`],
+  setupFilesAfterEnv: ['./setup-test-env.js'],
+};
